b4763b5f8dd9667d5e6e132d9f88eaf030b59840
Dmytrii Nagirniak <dnagir@gmail.com>
2011-12-20 13:48:46 +1100

    added jquery plugin

---
+++ vendor/assets/javascripts/spin-jquery.js
@@ -0,0 +1,22 @@
+// require jquery
+// require spin
+
+(function($) {
+
+  $.fn.spin = function(opts) {
+    this.each(function() {
+      var $this = $(this),
+          data = $this.data();
+
+      if (data.spinner) {
+        data.spinner.stop();
+        delete data.spinner;
+      }
+      if (opts !== false) {
+        data.spinner = new Spinner($.extend({color: $this.css('color')}, opts)).spin(this);
+      }
+    });
+    return this;
+  };
+
+})(jQuery);
